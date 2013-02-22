fe06d0757e09ac20232b9f1e81111c6c5a463d8f
Nick Moy <nick@chartbeat.com>
2013-02-22 14:03:34 -0500

    Added preferences to lint plugin allow ignoring certain warnings and changing max line length

--- closurelint.py
+++ closurelint.py
@@ -2,9 +2,24 @@ import sublime, sublime_plugin
 import subprocess
 import re
 
-def highlight_error(self, view, warning):
+LINE_LENGTH_REGEX = re.compile(r'Line too long \((\d+)')
+
+def highlight_error(self, view, warning, ignorelist, max_line_length):
   if warning:
     warning = warning.split(':')
+    # Ignore the lint warning if it matches one of our ignores from prefs
+    if ignorelist:
+      for ignore in ignorelist:
+        if ignore in warning[2]:
+          return None
+    # If this is a line length warning, test against preferred max line length
+    if max_line_length:
+      match = re.search(LINE_LENGTH_REGEX, warning[2])
+      if match:
+        length = int(match.group(1))
+        if length <= max_line_length:
+          return None
+
     line_number = int(warning[1]) - 1
     point = view.text_point(line_number, 0)
     line = view.line(point)
@@ -42,13 +57,16 @@ class ClosureLintListener(sublime_plugin.EventListener):
 
       if results:
         regions = []
+        ignorelist = view.settings().get("closurelint_ignore")
+        max_line_length = view.settings().get("closurelint_max_line_length")
+
         for line in results.split('\n'):
           if line.startswith(file_name):
-            region = highlight_error(self, view, line.replace(file_name, ''))
+            region = highlight_error(self, view, line.replace(file_name, ''), ignorelist, max_line_length)
             if region:
               regions.append(region)
 
-        view.add_regions('ClosureLintWarnings', regions, 'string', 'dot')
+        view.add_regions('ClosureLintWarnings', regions, 'string', 'dot', sublime.DRAW_OUTLINED)
 
 
   def on_selection_modified(self, view):
